452b972805278faa0d7a523267c37a1dedbfc064
marie_l <mariemlaqua@gmail.com>
2022-07-07 14:00:37 -0700

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,7 @@
+export const nameData = [
+	{ person: "Marge", id: "001" },
+	{ person: "Anthony", id: "002" },
+	{ person: "George", id: "003" },
+	{ person: "Mario", id: "004" },
+	{ person: "Saagar", id: "005" }
+];

3d84e1a32ef4a92147a86aa1d14aa60e8237c054
marie_l <mariemlaqua@gmail.com>
2022-07-30 08:46:13 -0700

    Add comments, reformat App 4 stateful component, delete data.js

--- src/data.js
+++
@@ -1,7 +0,0 @@
-export const nameData = [
-	{ person: "Marge", id: "001" },
-	{ person: "Anthony", id: "002" },
-	{ person: "George", id: "003" },
-	{ person: "Mario", id: "004" },
-	{ person: "Saagar", id: "005" }
-];
--- src/index.js
+++ src/index.js
@@ -1,27 +1,31 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
-import { nameData } from "./data.js";
 
 import "./styles.css";
-
-const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
 // Convert your App component from a functional component to a stateful component.
-class App extends Component {
-	const App = () => {
-		state = {value: 0 };
-		const NameTagElements = nameData.map(renderNameTag);
 
-		render () {
-			return (
-				<div className="App">
-					<h1>Name Tag Generator</h1>
-					{NameTagElements};
-				</div>
-			);
-		}:
-	};
-};
+// Removed unneeded additional ids
+const renderNameTag = (name) => <NameTag name={name} key={name} />;
+
+// Added the class component (same w/ the line 1 import React)
+class App extends Component {
+	// Added state replacing the need of the previous data.js file array via names array
+	state = { names: ["Bill", "Jim", "Jesus", "Mohammed", "Ziva"] }
+
+	// Added render & placed all the material / return w/i
+	render () {
+		const NameTagElements = this.state.names.map(renderNameTag);
+
+		return (
+			<div className="App">
+				<h1>Name Tag Generator</h1>
+				{NameTagElements};
+			</div>
+		);
+	}
+}
+
 
 
 const rootElement = document.getElementById("root");

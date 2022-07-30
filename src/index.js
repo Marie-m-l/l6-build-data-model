30adfb65924a85c30b0992dcd6a89b6f27bd1b93
marie_l <mariemlaqua@gmail.com>
2022-07-30 07:59:44 -0700

    Begin converting App component frm functional -> stateful by + component class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import NameTag from "./NameTag.js";
 import { nameData } from "./data.js";
@@ -6,17 +6,23 @@ import { nameData } from "./data.js";
 import "./styles.css";
 
 const renderNameTag = (name) => <NameTag name={name.person} key={name.id} />;
+// Convert your App component from a functional component to a stateful component.
+class App extends Component {
+	const App = () => {
+		state = {value: 0 };
+		const NameTagElements = nameData.map(renderNameTag);
 
-const App = () => {
-	const NameTagElements = nameData.map(renderNameTag);
-
-	return (
-		<div className="App">
-			<h1>Name Tag Generator</h1>
-			{NameTagElements};
-		</div>
-	);
+		render () {
+			return (
+				<div className="App">
+					<h1>Name Tag Generator</h1>
+					{NameTagElements};
+				</div>
+			);
+		}:
+	};
 };
+
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
